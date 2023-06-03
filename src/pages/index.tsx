a05bba1bcc597dab37edd0648058f1fc86cb1528
Luciara-Abreu <luciara.abreu@gmail.com>
2023-06-03 17:46:46 -0300

    inicial styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
+import Graduation from "@/components/graduation/Graduation";
 import Profile from "@/components/profile/Profile";
+import Skills from "@/components/skills/Skills";
+import Contacts from "@/components/contacts/Contacts"
+import Experiences from "@/components/experiences/Experiences";
+import Courses from "@/components/courses/Courses";
+import NameImage from "@/components/name-image/NameImage";
 
 export default function Home() {
   return (
-<div className="container-page">
-  <h1>Portifólio Luci Abreu - Next - React</h1>
-  <Profile/>
-</div>
-  
+    <div className="container-page">
+      <h1>Portifólio Luci Abreu - Next - React</h1>
+
+      <div className="left-side">
+        <NameImage />
+        <Graduation />
+        <Profile />
+        <Skills />
+        <Contacts />
+      </div>
+
+      <div className="right-side">
+        <Experiences/>
+        <Courses/>
+      </div>
+    </div>
   )
 }
