8d25c38880a3a1cea6763ae391149f3e90e53f3e
Luciara-Abreu <luciara.abreu@gmail.com>
2023-06-03 17:46:30 -0300

    add components

---
+++ src/components/contacts/Contacts.tsx
@@ -0,0 +1,43 @@
+function Contacts(){
+  return(
+    <div>
+        <article className="aritle-contat">
+        <img src="/src/assets/gifs/handshake.gif" className="contat-handshake" alt="Imagem ilustrativa de um arquivo"/>
+        <p className="p-contact">CONTATO</p>
+        <span className="linha-horizontal"></span>
+      </article>
+
+      <article className="article-links-contats">
+        <ul className="lista-contato">
+          <li className="li-fone">
+            <img src="assets/ringing.png" className="fone"/>
+            <a href="tel:+5551984539391" className="a-fone" target="_blank">Telefone Luci Abreu</a>
+          </li>
+
+          <li className="li-whats">
+            <img src="assets/whatsapp.png" className="whatsapp"/>
+            <a href="https://wa.me/5551984539391" className="a-whatsapp" target="_blank">WhatsApp Luci Abreu</a>
+          </li>
+
+          <li className="li-mail">
+            <img src="assets/envelope.png" className="email"/>
+            <a href="mailto:luciara.abreu@gmail.com.com" className="a-email" target="_blank">E-mail Luci Abreu</a>
+          </li>
+
+          <li className="li-git">
+            <img src="assets/github.png" className="git"/>
+            <a href="https://github.com/Luciara-Abreu" className="a-git" target="_blank">Repositório Luci Abreu</a>
+          </li>
+
+          <li className="li-in">
+            <img src="assets/linkedin.png" className="In"/>
+            <a href="https://www.linkedin.com/in/luciara-abreu/" className="a-In" target="_blank">LinkedIn Luci Abreu</a>
+          </li>
+
+        </ul>
+      </article>
+    </div>
+  )
+}
+
+export default Contacts
---
+++ src/components/name-image/NameImage.tsx
@@ -0,0 +1,14 @@
+function NameImage(){
+  return(
+    <div>
+      <header className="foto-e-name">
+        <img src="/src/assets/luciabreu.png" alt="minha imagem" className="minha-foto"/>
+        <h1 id="title">Luci Abreu</h1>
+        <div className="butterfly-container">
+          <div className="butterfly"></div>
+        </div>
+      </header>
+    </div>
+  )
+}
+export default NameImage
