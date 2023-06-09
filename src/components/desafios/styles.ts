1f9a8bbad97f70d979807a149822257f80e7b5ba
Luciara-Abreu <luciara.abreu@gmail.com>
2023-06-09 16:44:41 -0300

    add new functions

--- src/components/desafios/styles.ts
+++ src/components/desafios/styles.ts
@@ -1,23 +1,27 @@
 import styled from 'styled-components'
 
 
-const ContainerProjetos = styled.div`
+const ContainerDesafios = styled.div`
 width: 100%;
 `
 
 const ContainerLinks = styled.div`
+    position: relative;
     width: 100%;
     display: flex;
     justify-content: flex-start;
     align-items: center; 
 
-
+ 
   .a-projeto {
     text-decoration: none;
     color: aliceblue;
     margin: 0;
     padding: 0% 0 5% 0;
     }
+
+    //-----------------------------------------------------------
+
 
     @media (max-width: 767px) {
       width: auto;
@@ -31,10 +35,10 @@ const ContainerLinks = styled.div`
     margin: 0;
     padding: 0% 0 5% 5%;
     }
-
-    }
+  }
   `
+
 export {
-  ContainerProjetos,
+  ContainerDesafios,
   ContainerLinks
 }
--- src/components/projetos/Projetos.tsx
+++ src/components/projetos/Projetos.tsx
@@ -1,26 +1,27 @@
 import { ContainerComponent, HorizontalLine, ImageContainer } from '@/styles/styles'
 import Image from 'next/image'
 import { ContainerLinks, ContainerProjetos } from './stayles'
+import Borboleta from '../borboleta/Borboleta'
 
 
-function Projetos(){
-  return(
+function Projetos() {
+  return (
     <ContainerComponent>
       <ImageContainer>
         <Image src="/images/gifs/steps.gif" className="image-container" alt="Imagem ilustrativa de um arquivo" width={0} height={0} />
         <p className="p-subtitle">PROJETOS</p>
         <HorizontalLine />
-      </ImageContainer>  
+      </ImageContainer>
 
       <ContainerProjetos>
-      <ContainerLinks>
-          <Image src="/images/borboleta.png" className="In" alt="imagem LnkedIn" width={50} height={50} />
-          <a href="#" className="a-In" target="_blank">Agenda Aniversariantes</a>
+        <ContainerLinks>
+          <Borboleta />
+          <a href="#" className="a-projeto" target="_blank">Agenda Aniversariantes</a>
         </ContainerLinks>
 
         <ContainerLinks>
-          <Image src="/images/borboleta.png" className="In" alt="imagem LnkedIn" width={50} height={50} />
-          <a href="#" className="a-In" target="_blank">Calculadora 3D</a>
+          <Borboleta />
+          <a href="#" className="a-projeto" target="_blank">Calculadora 3D</a>
         </ContainerLinks>
       </ContainerProjetos>
 
