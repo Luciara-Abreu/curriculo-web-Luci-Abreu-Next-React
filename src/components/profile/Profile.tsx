6678c6ebb1140b8832331d8785918f3efb299291
Luciara-Abreu <luciara.abreu@gmail.com>
2023-06-08 22:46:11 -0300

    update

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -11,8 +11,8 @@ function Profile() {
       </ImageContainer>
 
       <p>
-        Eu possuo habilidades técnicas em programação de nível Pleno, o que me permite criar algumas soluções de software
-        e resolver alguns problemas através da codificação. Além disso, tenho conhecimento em Containerersas linguagens de desenvolvimento
+        Eu possuo habilidades técnicas em programação de nível pleno, o que me permite criar algumas soluções de software
+        e resolver alguns problemas através da codificação. Além disso, tenho conhecimento em linguagens de desenvolvimento
         e estou familiarizado com alguns frameworks populares. Também possuo experiência em bancos de dados e conhecimento em seus
         frameworks. Sou uma solucionadora de problemas com pensamento lógico e uma boa capacidade de trabalho em equipe. Estou constantemente
         atualizado com as melhores práticas e metodologias ágeis. Busco sempre aprender e me adaptar às novas tecnologias e tendências do setor.
